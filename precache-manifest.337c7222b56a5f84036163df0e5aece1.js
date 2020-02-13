11a9ee853ac7b30e3e11b37ffeddc8111215d064
Iddar Olivares <iddar@dbug.mx>
2020-02-13 13:03:08 -0600

    Updates

---
+++ precache-manifest.337c7222b56a5f84036163df0e5aece1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfbd11fc1057d2042aaa971159e6441b",
+    "url": "/shappes-demo/index.html"
+  },
+  {
+    "revision": "cbdf342ed977a2167e46",
+    "url": "/shappes-demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "58278095701a4abee098",
+    "url": "/shappes-demo/static/js/2.15dee995.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/shappes-demo/static/js/2.15dee995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbdf342ed977a2167e46",
+    "url": "/shappes-demo/static/js/main.f8b5dba4.chunk.js"
+  },
+  {
+    "revision": "1c151c7523ef529a7cec",
+    "url": "/shappes-demo/static/js/runtime-main.fd89169e.js"
+  }
+]);
